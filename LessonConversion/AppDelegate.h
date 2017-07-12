16d69c7f25e96cad1aa89e218d42cc2832ca9eaa
泽秦  严 <854386200@qq.com>
2017-07-12 22:09:06 +0800

    Initial Commit

---
+++ LessonConversion/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LessonConversion
+//
+//  Created by 泽秦  严 on 2017/7/12.
+//  Copyright © 2017年 泽秦  严. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
